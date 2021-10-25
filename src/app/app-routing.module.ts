91206afa1d10ce52dc8dfff2367949b2baf4f777
DESKTOP-7LN1193\niraj <nirajdahal09@gmail.com>
2021-10-25 23:16:34 +0545

    Added logout functionality and auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 import { TestComponent } from './feature/components/test/test.component';
 
 const routes: Routes = [
-  {path:'',component:TestComponent},
+  {path:'',component:TestComponent, canActivate:[AuthGuard]},
   { path: 'account', loadChildren: () => import('./feature/components/account/account.module').then(mod => mod.AccountModule) }
 ];
 
--- src/app/feature/components/account/account.service.ts
+++ src/app/feature/components/account/account.service.ts
@@ -65,7 +65,10 @@ export class AccountService {
     const email = this.decodeJwtToken()['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'];
     return email;
   }
-
+  public getCurrentUserRole() {
+    const role = this.decodeJwtToken()['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+    return role;
+  }
   private decodeJwtToken() {
     const token = localStorage.getItem("token");
     const decodedToken = this._jwtHelper.decodeToken(token!);
